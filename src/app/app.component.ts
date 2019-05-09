3bae3ddffa625508f10f27426efe2521ddfcf001
KevinSolimo <kevinsolimo@gmail.com>
2019-05-09 09:50:26 +0000

    Menu-IconLogin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,54 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Mono-Rent';
 
-
-  //Cordinate Milano 45,4773; 9,1815
-  lat: number = 45.4773;
-  lng: number = 9.1815;
-
-  constructor() { this.findMe(); this.calcoloDistanza(); }
-
-  calcoloDistanza() {
-
-    //Punto A 45.522457, 9.209638
-    //Punto B 45.501299, 9.197877
-    
-    this.distanceInKmBetweenEarthCoordinates(45.522457, 9.209638,45.501299, 9.197877);
-
-  }
-
-  degreesToRadians(degrees) {
-    return degrees * Math.PI / 180;
-  }
-
-  distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
-
-    const earthRadiusKm = 6371;
-
-    var dLat = this.degreesToRadians(lat2 - lat1);
-    var dLon = this.degreesToRadians(lon2 - lon1);
-
-    lat1 = this.degreesToRadians(lat1);
-    lat2 = this.degreesToRadians(lat2);
-
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    return (earthRadiusKm * c);
-
-  }
-
-  findMe() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.lat = position.coords.latitude;
-        this.lng = position.coords.longitude;
-      });
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,24 +9,24 @@ import { AppComponent } from './app.component';
 //QRCode Reader
 
 //Material Design Animation
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //Material Design Component
-import {MatButtonModule} from '@angular/material';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatButtonModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//Http Import
+import { HttpClientModule } from '@angular/common/http';
 
 //Component
 import { ReaderQRComponent } from './reader-qr/reader-qr.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
-//Http Import
-import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 
 //Maps AGM
@@ -34,7 +34,8 @@ import { AgmCoreModule } from '@agm/core';
 
 //Routing delle pagine
 const appRoutes: Routes = [
-  { path: 'register', component:  RegisterComponent}
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
